fa855a55ff9ba83a8d83f0cc0769b6fbb64694b0
sulekha karmakar <72262278+pro-sulekha-coder@users.noreply.github.com>
2020-10-02 20:25:46 +0530

    Add files via upload
    this is helloworld in java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class helloworld
+{//main function
+	public static void main(String[] args) 
+	{
+		//Scanner sc=new Scanner(System.in);
+		System.out.println("Hello FUCKING world...");
+	}
+}
+//end of the test code...
+	
